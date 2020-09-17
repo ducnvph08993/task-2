542d772e32af9f4c267914f783170b3d27f6a7aa
ducnvph08993 <57821959+ducnvph08993@users.noreply.github.com>
2020-09-17 15:50:16 +0700

    rt

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { useState } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route, HashRouter } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, HashRouter } from 'react-router-dom';
 import ShoppingList from './components/ShoppingList';
 import Detail from './components/Detail'
 import dataFake from './dataFake';
 
 function App() {
-  console.log("a:" + process.env.PUBLIC_URL);
   const [items, setItems] = useState(dataFake);
   const onHandleAdd = (item) => {
     console.log(item);
@@ -23,7 +22,7 @@ function App() {
   const Abc = function () {
     return <h1>ABC</h1>;
   }
-
+  console.log(process.env.PUBLIC_URL);
   return (
     <div className="App">
       <HashRouter basename={process.env.PUBLIC_URL}>
